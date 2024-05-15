9ad0b79d9dd5ab5a4c400652379564646ba1c4b4
jennn-arce <j.arce@uniandes.edu.co>
2024-05-15 08:21:34 -0500

    class paises

---
+++ appparcial/src/app/paises/paises.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaisesComponent } from './paises.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaisesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaisesModule { }
