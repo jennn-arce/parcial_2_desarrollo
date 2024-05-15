93b78b9a8aaba787a43bd8096a15ba0f3af02841
jennn-arce <j.arce@uniandes.edu.co>
2024-05-15 09:42:27 -0500

    se ve horrible pero funciona

--- appparcial/src/app/paises/paises-detail/paises-detail.component.ts
+++ appparcial/src/app/paises/paises-detail/paises-detail.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Paises } from '../paises';
 
 @Component({
   selector: 'app-paises-detail',
   templateUrl: './paises-detail.component.html',
   styleUrl: './paises-detail.component.css'
 })
-export class PaisesDetailComponent {
+export class PaisesDetailComponent implements OnInit{
+
+  
+  @Input() paisesDetail!: Paises;
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
